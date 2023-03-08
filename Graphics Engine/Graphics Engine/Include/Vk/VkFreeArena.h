2d795262d21050d4f8dc63c6cc110189d8a39892
Jan <jan.vijfhuizen@outlook.com>
2023-03-08 20:31:50 +0100

    [WIP] VkFreeArena

---
+++ Graphics Engine/Graphics Engine/Include/Vk/VkFreeArena.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+#include "JLib/Array.h"
+#include "JLib/LinkedList.h"
+
+namespace jv::vk
+{
+	struct App;
+
+	struct FreeArenaInfo final
+	{
+		Arena* arena;
+		App* app;
+		uint32_t pageSize = 4096;
+	};
+
+	struct FreeArena final
+	{
+		struct Pool final
+		{
+			VkFlags memPropertyFlags;
+		};
+
+		struct Page final
+		{
+			uint32_t size;
+		};
+
+		FreeArenaInfo info;
+		ArenaScope scope;
+		Array<Pool> pools;
+		LinkedList<Page> pages{};
+
+		static FreeArena Create(const FreeArenaInfo& info);
+		static void Destroy(const FreeArena& freeArena);
+	};
+}
---
+++ Graphics Engine/Graphics Engine/Src/Vk/VkFreeArena.cpp
@@ -0,0 +1,32 @@
+﻿#include "pch.h"
+#include "Vk/VkFreeArena.h"
+#include "JLib/ArrayUtils.h"
+#include "Vk/VkApp.h"
+
+namespace jv::vk
+{
+	FreeArena FreeArena::Create(const FreeArenaInfo& info)
+	{
+		FreeArena freeArena{};
+		freeArena.info = info;
+		freeArena.scope = ArenaScope::Create(*info.arena);
+
+		VkPhysicalDeviceMemoryProperties memProperties;
+		vkGetPhysicalDeviceMemoryProperties(info.app->physicalDevice, &memProperties);
+		freeArena.pools = CreateArray<Pool>(*info.arena, memProperties.memoryTypeCount);
+
+		for (uint32_t i = 0; i < memProperties.memoryTypeCount; ++i)
+		{
+			auto& pool = freeArena.pools[i];
+			const auto& memType = memProperties.memoryTypes[i];
+			pool.memPropertyFlags = memType.propertyFlags;
+		}
+
+		return freeArena;
+	}
+
+	void FreeArena::Destroy(const FreeArena& freeArena)
+	{
+		ArenaScope::Destroy(freeArena.scope);
+	}
+}
