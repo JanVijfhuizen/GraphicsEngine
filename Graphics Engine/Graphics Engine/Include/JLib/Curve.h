c3194b6764ef4450183210ec212e0ed39bc3ee40
Jan <jan.vijfhuizen@outlook.com>
2023-08-03 21:36:48 +0200

    [Add] Bezier curve.

---
+++ Graphics Engine/Graphics Engine/Include/JLib/Curve.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+namespace je
+{
+	// Tool to generate cubic bezier curves with:
+	// https://cubic-bezier.com
+
+	// Cubic Bezier Curve, to be exact.
+	struct Curve final
+	{
+		glm::vec2 points[2]{};
+
+		// Evaluates the curve at the target point between 0 and 1.
+		[[nodiscard]] float Evaluate(float lerp) const;
+	};
+
+	// Evaluates from two seperate curves, one for the climb and one for the drop.
+	[[nodiscard]] float DoubleCurveEvaluate(float lerp, const Curve& up, const Curve& down);
+
+	// Creates a curve that moves from 0 to 1 vertically, and slightly overshoots right before the end.
+	[[nodiscard]] Curve CreateCurveOvershooting();
+	// Creates a curve that quickly moves to the endpoint, and slows down at the end.
+	[[nodiscard]] Curve CreateCurveDecelerate();
+}
