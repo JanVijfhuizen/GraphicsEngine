8aadb045eea4dede71a07b58b98f4261a9f496b9
Jan <jan.vijfhuizen@outlook.com>
2023-09-14 09:26:08 +0200

    [Fix] Didnt save header.

--- GraphicsEngine/Game/Include/Utils/NormalMapGenerator.h
+++ GraphicsEngine/Game/Include/Utils/NormalMapGenerator.h
@@ -1,6 +1,7 @@
 ﻿#pragma once
+#include "JLib/Array.h"
 
 namespace game
 {
-	unsigned char* GenerateNormalMap(unsigned char* heightMap, glm::ivec2 resolution);
+	[[nodiscard]] jv::Array<unsigned char> GenerateNormalMap(const unsigned char* heightMap, glm::ivec2 resolution, jv::Arena& arena);
 }
