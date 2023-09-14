86220f6b331afae10f3f232aa3877d7006dcfe89
Jan <jan.vijfhuizen@outlook.com>
2023-09-14 09:32:18 +0200

    [Remove] Normal map generator.
    There are better alternatives in-browser.

--- GraphicsEngine/Game/Include/Utils/NormalMapGenerator.h
+++
@@ -1,7 +0,0 @@
-﻿#pragma once
-#include "JLib/Array.h"
-
-namespace game
-{
-	[[nodiscard]] jv::Array<unsigned char> GenerateNormalMap(const unsigned char* heightMap, glm::ivec2 resolution, jv::Arena& arena);
-}
