f7f03609d4f79a0094fbb52e1b63cbfeb0e9ed8a
Jan <jan.vijfhuizen@outlook.com>
2024-01-01 16:39:43 +0100

    [Change] Balancing.

--- GraphicsEngine/Game/Src/Levels/MainLevel.cpp
+++ GraphicsEngine/Game/Src/Levels/MainLevel.cpp
@@ -186,7 +186,7 @@ namespace game
 
 		if (!bossPresent)
 		{
-			const auto enemyCount = jv::Min<uint32_t>(jv::Min<uint32_t>(2 + state.depth / ROOM_COUNT_BEFORE_BOSS, 5), state.depth + 1);
+			const auto enemyCount = jv::Min<uint32_t>(jv::Min<uint32_t>(2 + state.depth / ROOM_COUNT_BEFORE_BOSS, 4), state.depth + 1);
 			for (uint32_t i = 0; i < enemyCount; ++i)
 			{
 				const uint32_t id = state.GetMonster(info);
